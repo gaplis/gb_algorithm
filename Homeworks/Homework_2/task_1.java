c510bb9e626597d3898ed87734103feea5fb4770
IlyaChistov <gaplis322@gmail.com>
2023-04-27 19:39:17 +0300

    Task complited

---
+++ Homeworks/Homework_2/task_1.java
@@ -0,0 +1,51 @@
+package Homeworks.Homework_2;
+
+public class task_1 {
+    public static void main(String[] args) {
+        int[] array = new int[] {
+            4, 2, 5, 8, 1, 9, 2, 3, 6, 8, 5
+        };
+
+        sort(array);
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+    public static void sort(int[] array){
+        
+        // построение кучи (перегрупируем массив)
+        for (int i = array.length / 2 - 1; i >= 0; i--) {
+            heapify(array, array.length, i);        
+        }
+
+        // Один за другим извлекаем элементы из кучи
+        for (int i = array.length - 1; i >= 0; i--) {
+            // Перемещаем текущий корень в конец
+            int temp = array[0];
+            array[0] = array[i];
+            array[i] = temp;
+
+            // Вызываем процедуру heapify на уменьшенной куче
+            heapify(array, i, 0);
+        }
+    }
+
+    private static void heapify(int[] array, int heapSize, int rootIndex) {
+        int largest = rootIndex; // инициализируем наибольший эллемент как корень
+        int leftChild = 2 * rootIndex + 1;
+        int rightChild = 2 * rootIndex + 2;
+
+        if (leftChild < heapSize && array[leftChild] > array[largest]) largest = leftChild;
+
+        if (rightChild < heapSize && array[rightChild] > array[largest]) largest = rightChild;
+
+        if (largest != rootIndex) {
+            int temp = array[rootIndex];
+            array[rootIndex] = array[largest];
+            array[largest] = temp;
+
+            heapify(array, heapSize, largest);
+        }
+    }
+}
